3d529dcfe0e4b0fc54462b427d83b2f391d4f751
venkata manikantha <venkatmanikantha1@gmail.com>
2024-10-23 12:07:29 +0530

    merge resolved

--- src/app.js
+++ src/app.js
@@ -1,20 +1,29 @@
 const express = require('express');
-const crmRoutes = require('./routes/crmroutes');
-const marketingRoutes = require('./routes/marketingroute');
+const connectDB = require('./config/db'); // MongoDB connection
+const crmRoutes = require('./routes/crmRoutes');
+const marketingRoutes = require('./routes/marketingRoutes');
+const reportRoutes = require('./routes/reportRoutes');
 
 const app = express();
+
+connectDB();
 
 app.use(express.json());
 
 app.use('/api/crm', crmRoutes);
-
 app.use('/api/marketing', marketingRoutes);
+app.use('/api/reports', reportRoutes);
 
 app.get('/', (req, res) => {
-  res.send('Welcome to EzyMetrics API Service');
+  res.send('Welcome to EzyMetrics API');
 });
 
-const PORT =  5000;
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send({ message: 'Server Error' });
+});
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
